76d11a18aea8def640a935e4ee6e9271a21573a9
Arnav D <98266168+D-Arnav@users.noreply.github.com>
2025-01-31 17:57:45 +0530

    Update train.py

--- utils/train.py
+++ utils/train.py
@@ -128,12 +128,6 @@ def train_sfda2(classifier, target_train_dl, target_test_dl, source_test_dl, con
 class CrossEntropyLabelSmooth(nn.Module):
     """
     Cross entropy loss with label smoothing regularizer.
-    Reference:
-    Szegedy et al. Rethinking the Inception Architecture for Computer Vision. CVPR 2016.
-    Equation: y = (1 - epsilon) * y + epsilon / K.
-    Args:
-        num_classes (int): number of classes.
-        epsilon (float): weight.
     """
 
 
